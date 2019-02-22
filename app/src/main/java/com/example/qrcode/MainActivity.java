3dcadf1518965b5364c3aeb217e3b7c130c40c42
BGuenego <beatrice.guenego@gmail.com>
2019-02-22 09:34:47 +0100

    DisplayTestActivity : creation d une activite destinee au test sur tel des classes
    Signed-off-by: BGuenego <beatrice.guenego@gmail.com>

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.qrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  public static final String EXTRA_MESSAGE = "com.example.qrcode.MESSAGE";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+  }
+
+  /** Fonction de test de QrRead et QrMessage */
+  public void sendMessage(View view) {
+      Intent intent = new Intent(this, DisplayTestActivity.class);
+      String msg = "Test de l'activité";// TODO
+      intent.putExtra(EXTRA_MESSAGE, msg);
+      startActivity(intent);
+
+  }
+
+
 }
