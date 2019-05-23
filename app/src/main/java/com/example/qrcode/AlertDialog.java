41c6299583b78816b5481408b7fa6db129b18bc7
Pierre-Hugues Blelly <pierre-hugues.blelly@supelec.fr>
2019-05-23 12:07:46 +0200

    Copie le mesage dans le clipboard

--- app/src/main/java/com/example/qrcode/AlertDialog.java
+++ app/src/main/java/com/example/qrcode/AlertDialog.java
@@ -5,20 +5,38 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.app.AlertDialog.Builder;
 
 import android.content.ClipboardManager;
 
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+
+
 
 public class AlertDialog extends AppCompatDialogFragment {
 
     private String text;
 
+    public AlertDialog()
+    {
+        super();
+        this.text = "Error";
+    }
+
     public AlertDialog(String text) {
         super();
         this.text = text;
+
+    }
+    public String getText()
+    {
+        return this.text;
     }
 
     public Dialog onCreateDialog(Bundle savedInstanceState){
@@ -31,20 +49,22 @@ public class AlertDialog extends AppCompatDialogFragment {
         builder.setPositiveButton("Copy", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                System.out.println("Je copie mon truc");
-
-                // On récupère le clipboard manager
+                String text = AlertDialog.this.getText();
                 /*
-                final android.content.ClipboardManager clipboardManager = (ClipboardManager) myContext.getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("Source Text", text);
-                clipboardManager.setPrimaryClip(clipData);
+                if ("http".equals(text.substring(0,4)))
+                {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(text));
+                    startActivity(browserIntent);
+                }
                 */
+                ClipboardManager manager = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Results", text);
+                manager.setPrimaryClip(clip);
             }
         });
         builder.setNegativeButton("Quit", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
             }
         });
         return builder.create();
