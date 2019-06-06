7b47654f6156e1aad21a313ef46c9343d798f72d
Benjamin <benjamin.hauguel@orange.fr>
2019-06-06 19:53:38 +0200

    corrigé android manifest, et qq icones qui marchaient pas

--- openCVLibrary343/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary343/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/beatrice/AndroidStudioProjects/QrCode/openCVLibrary343/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: D:\\Program Files\\Android\\Projects\\QrCodeFinal\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
